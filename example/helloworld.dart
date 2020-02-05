9e702b36e5406e96a297f7470bf77f8df18b318e
andrew <andrew@textile.io>
2020-02-04 18:40:13 -0800

    examples: fix import naming
    Signed-off-by: andrew <andrew@textile.io>

--- example/helloworld.dart
+++ example/helloworld.dart
@@ -1,13 +1,13 @@
 
 import 'package:textile/textile.dart';
-import 'package:threads_client/threads_client.dart';
+import 'package:threads_client/threads_client.dart' as threads;
 
 const APP_TOKEN = '<app token>';
 const DEVICE_ID = '<uuid>';
 
 void main(List<String> args) async {
   final config = ThreadsConfig(APP_TOKEN, DEVICE_ID);
-  final client = ThreadsClient(config: config);
+  final client = threads.Client(config: config);
   final store = await client.newStore();
   print('New store $store');
 }
