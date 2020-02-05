702fd374eccee194d900b1e7e0c40215ef48a59a
andrew <andrew@textile.io>
2020-02-05 11:00:34 -0800

    tests: clean up some comments
    Signed-off-by: andrew <andrew@textile.io>

--- test/textile_test.dart
+++ test/textile_test.dart
@@ -6,13 +6,11 @@ void main() async {
   textile.API api;
   final env = Platform.environment;
   setUpAll(() {
-    // Create a new threads client
     final host = env.containsKey('THREADS_HOST') && env['THREADS_HOST'] != '' ? env['THREADS_HOST'] : '127.0.0.1';
     final port = env.containsKey('THREADS_PORT') && env['THREADS_PORT'] != '' ? int.parse(env['THREADS_PORT']) : 6006;
     api = textile.API('<app token>', '<user id>', dev: true, api: host, threadsPort: port);
   });
   tearDownAll(() async {
-    // Shutdown the threads client.
     await api.shutdown();
   });
   test('Create & start a new data store', () async {
