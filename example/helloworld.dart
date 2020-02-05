3b90a79a92b6dc372226eec2cc3a0777279237f6
andrew <andrew@textile.io>
2020-02-04 18:34:46 -0800

    examples: fix example class type
    Signed-off-by: andrew <andrew@textile.io>

--- example/helloworld.dart
+++ example/helloworld.dart
@@ -6,7 +6,7 @@ const APP_TOKEN = '<app token>';
 const DEVICE_ID = '<uuid>';
 
 void main(List<String> args) async {
-  final config = TextileThreadsConfig(APP_TOKEN, DEVICE_ID);
+  final config = ThreadsConfig(APP_TOKEN, DEVICE_ID);
   final client = ThreadsClient(config: config);
   final store = await client.newStore();
   print('New store $store');
--- lib/src/threads.dart
+++ lib/src/threads.dart
@@ -1,9 +1,4 @@
-import 'dart:async';
-import 'dart:convert';
-import 'package:grpc/grpc.dart';
 import 'package:threads_client/threads_client.dart' as threads;
-import 'package:http/http.dart' as http;
-import 'package:googleapis_auth/auth_io.dart' as auth;
 import 'auth.dart';
 import 'defaults.dart';
 
