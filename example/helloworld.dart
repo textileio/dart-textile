31cb8eea9b5f75df1de9525fed8ad2cf2de090f0
andrew <andrew@textile.io>
2020-02-04 18:40:57 -0800

    examples: make textile method more clear in example
    Signed-off-by: andrew <andrew@textile.io>

--- example/helloworld.dart
+++ example/helloworld.dart
@@ -1,12 +1,12 @@
 
-import 'package:textile/textile.dart';
+import 'package:textile/textile.dart' as textile;
 import 'package:threads_client/threads_client.dart' as threads;
 
 const APP_TOKEN = '<app token>';
 const DEVICE_ID = '<uuid>';
 
 void main(List<String> args) async {
-  final config = ThreadsConfig(APP_TOKEN, DEVICE_ID);
+  final config = textile.ThreadsConfig(APP_TOKEN, DEVICE_ID);
   final client = threads.Client(config: config);
   final store = await client.newStore();
   print('New store $store');
